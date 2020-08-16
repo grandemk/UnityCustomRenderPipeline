b9bb059b0f789e9cf11c0efc72b4cc7c348028db
Kevin Grandemange <grandemange.kevin@gmail.com>
2020-08-16 19:08:51 +0200

    First import

---
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+    ScriptableRenderContext context;
+    Camera camera;
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer { name = bufferName };
+    CullingResults cullingResults;
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+
+    void Setup()
+    {
+        // view projection matrix: combines the camera's position and orientation and the perspective / orthographic projection
+        // in Shaders; unity_MatrixVP
+        context.SetupCameraProperties(camera);
+        CameraClearFlags flags = camera.clearFlags;
+        // If we are clearing with a color, use the linear version as we do everything with linear here
+        buffer.ClearRenderTarget(flags <= CameraClearFlags.Depth, flags == CameraClearFlags.Color, flags == CameraClearFlags.Color ? camera.backgroundColor.linear : Color.clear);
+        buffer.BeginSample(SampleName);
+        ExecuteBuffer();
+    }
+
+    // Draw all geometry that the camera can see.
+    void DrawVisibleGeometry()
+    {
+        var sortingSettings = new SortingSettings(camera) { criteria = SortingCriteria.CommonOpaque };
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+        context.DrawSkybox(camera);
+
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+
+    void Submit()
+    {
+        buffer.EndSample(SampleName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        PrepareBuffer();
+        PrepareForSceneWindow();
+        if (!Cull())
+            return;
+
+        Setup();
+        DrawVisibleGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        Submit();
+    }
+}
---
+++ Assets/Custom RP/Runtime/CustomRenderPipelineAsset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+// Get a hold of a pipeline object instance that is responsible for rendering
+[CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
+public class CustomRenderPipelineAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new CustomRenderPipeline();
+    }
+}
